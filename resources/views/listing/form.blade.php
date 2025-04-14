303ded8bd3e8da716640697a8d250f80d1020eff
Kolby Dunning <kolby@kolbyd.ca>
2025-04-13 23:05:49 -0400

    fix: update listing pages

--- resources/views/listing/form.blade.php
+++ resources/views/listing/form.blade.php
@@ -4,27 +4,28 @@
 @section('content')
 <div class="w-full">
     <div class="flex flex-col mb-4">
-        <a class="flex flex-row items-center gap-2 text-red-300 cursor-pointer" href="{{ route('listing.poster-listings') }}">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
-            </svg>
-            Back to Listings
+        <a class="mb-2" href="{{ route('listing.poster-listings') }}">
+            <button class="flex flex-row items-center gap-2 bg-red-500 font-bold uppercase text-white px-4 py-2 hover:bg-black transition duration-500 ease-in-out cursor-pointer">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="4" stroke="currentColor" class="size-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
+                </svg>
+                Back to Listings
+            </button>
         </a>
-        <p class="text-2xl font-bold">{{ isset($listing) ? "Edit Listing" : "Create Listing" }}</p>
+        <p class="text-2xl font-bold uppercase">{{ isset($listing) ? "Edit Listing" : "Create Listing" }}</p>
     </div>
     @if(isset($listing))
-        {{-- TODO: Show interested users here --}}
         <div class="flex flex-col mb-4">
-            <button id="interested-users-button" class="text-black bg-red-300 hover:bg-red-400 transition-colors transition-300 px-4 py-1 rounded mr-2 cursor-pointer" onclick="changeInterestedUsersDropdown()">
+            <button id="interested-users-button" class="bg-red-500 font-bold uppercase text-white px-4 py-2 hover:bg-black transition duration-500 ease-in-out cursor-pointer" onclick="changeInterestedUsersDropdown()">
                 Show Interested Users
             </button>
             <div id="interested-users-dropdown" class="hidden">
                 @if($listing->interestedUsers->isEmpty())
-                    <p class="text-gray-400">No users have shown interest in this listing.</p>
+                    <p class="text-gray-500">No users have shown interest in this listing.</p>
                 @else
-                    <ul class="list-inside text-gray-300 list-none sm:columns-2 lg:columns-4 text-center">
+                    <ul class="list-inside text-blue-700 list-none sm:columns-2 lg:columns-4 text-center">
                         @foreach($listing->interestedUsers as $user)
-                            <li><a href="mailto:{{$user->username}}" class="text-red-300 hover:text-red-400 transition-colors transition-200 cursor-pointer">{{ $user->name }}</a></li>
+                            <li><a href="mailto:{{$user->username}}" class="hover:text-blue-900 cursor-pointer">{{ $user->name }}</a></li>
                         @endforeach
                     </ul>
                 @endif
@@ -39,18 +40,18 @@
         <div class="flex flex-col mb-4">
             <label for="title" class="text-lg font-bold mb-2">Title</label>
 
-            <input type="text" name="title" id="title" value="{{ old('title', $listing->title ?? '') }}" class="p-2 rounded bg-gray-700 text-white border border-gray-400 focus:outline-none focus:ring focus:ring-red-400" required>
+            <input type="text" name="title" id="title" value="{{ old('title', $listing->title ?? '') }}" class="p-2 bg-red-100 focus:outline-none focus:ring focus:ring-red-400" required>
             @error('title')
                 <span class="text-red-500 text-sm mt-1">{{ $message }}</span>
             @enderror
 
             <label for="body" class="text-lg font-bold mb-2 mt-4">Body</label>
-            <textarea name="body" id="body" rows="5" class="p-2 rounded bg-gray-700 text-white border border-gray-400 focus:outline-none focus:ring focus:ring-red-400" required>{{ old('body', $listing->body ?? '') }}</textarea>
+            <textarea name="body" id="body" rows="5" class="p-2 bg-red-100 focus:outline-none focus:ring focus:ring-red-400" required>{{ old('body', $listing->body ?? '') }}</textarea>
             @error('body')
                 <span class="text-red-500 text-sm mt-1">{{ $message }}</span>
             @enderror
 
-            <button type="submit" class="font-bold bg-red-400 text-black rounded px-4 py-1 mt-4 hover:bg-red-500 transition duration-300 ease-in-out cursor-pointer">
+            <button type="submit" class="mt-4 bg-red-500 font-bold uppercase text-white px-4 py-2 hover:bg-black transition duration-500 ease-in-out cursor-pointer">
                 Save
             </button>
         </div>
--- resources/views/listing/poster-index.blade.php
+++ resources/views/listing/poster-index.blade.php
@@ -6,22 +6,22 @@
         <div>
             <p class="text-2xl font-bold">My Listings</p>
             @role(\App\Models\RoleAssignment::ADMIN_ROLE)
-            <p class="text-xs text-gray-400">Since you are an admin, you can see all listings!</p>
+            <p class="text-xs text-gray-500">Since you are an admin, you can see all listings!</p>
             @endrole
         </div>
         <a href="{{ route('listing.create') }}" class="ml-auto justify-center h-fit my-auto">
-            <button class="bg-red-400 text-black rounded px-4 py-2 hover:bg-red-500 transition duration-300 ease-in-out cursor-pointer">
+            <button class="bg-red-500 font-bold uppercase text-white px-4 py-2 hover:bg-black transition duration-500 ease-in-out cursor-pointer">
                 Create New Listing
             </button>
         </a>
     </div>
     <table class="w-full h-fit rounded border-spacing-2 border-collapse">
         <thead>
-            <tr class="bg-gray-700">
-                <th class="text-left p-4 border border-gray-400">Title</th>
-                <th class="text-left p-4 border border-gray-400">Interested</th>
-                <th class="text-left p-4 border border-gray-400">Created</th>
-                <th class="text-center p-4 border border-gray-400">Actions</th>
+            <tr class="bg-red-500 uppercase font-bold text-white">
+                <th class="text-left p-4 border border-red-800">Title</th>
+                <th class="text-left p-4 border border-red-800">Interested</th>
+                <th class="text-left p-4 border border-red-800">Created</th>
+                <th class="text-center p-4 border border-red-800">Actions</th>
             </tr>
         </thead>
         <tbody>
@@ -31,8 +31,8 @@
                     $expired = $listing->created_at->lt(now()->subMonths(2));
                 @endphp
 
-                <tr class="{{ $expired ? 'bg-red-200 text-black' : 'text-gray-400' }}">
-                    <td class="p-2 px-4 border border-gray-400">
+                <tr class="{{ $expired ? 'bg-red-200 text-black' : 'text-gray-700' }}">
+                    <td class="p-2 px-4 border border-red-500">
                         <div class="flex flex-row">
                             @if( $expired )
                                 <p class="text-red-500 font-bold mr-2">EXPIRED</p>
@@ -40,15 +40,15 @@
                             <p>{{ $listing->title }}</p>
                         </div>
                     </td>
-                    <td class="p-2 px-4 border border-gray-400">{{ $listing->interestedUsers()->count() }}</td>
-                    <td class="p-2 px-4 border border-gray-400">{{ $listing->created_at->format('Y-m-d H:i') }}</td>
-                    <td class="p-2 px-4 border border-gray-400">
-                        <div class="flex flex-col lg:flex-row justify-around {{ $expired ? 'text-blue-700' : 'text-red-300' }}">
-                            <a href="{{ route('listing.edit', $listing->id) }}" class="cursor-pointer">View/Edit</a>
+                    <td class="p-2 px-4 border border-red-500">{{ $listing->interestedUsers()->count() }}</td>
+                    <td class="p-2 px-4 border border-red-500">{{ $listing->created_at->format('Y-m-d H:i') }}</td>
+                    <td class="p-2 px-4 border border-red-500">
+                        <div class="flex flex-col lg:flex-row justify-around text-blue-700">
+                            <a href="{{ route('listing.edit', $listing->id) }}" class="cursor-pointer hover:text-blue-900">View/Edit</a>
                             <form method="POST" action="{{ route('listing.destroy', $listing->id) }}" class="ml-2">
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit" class="cursor-pointer">Delete</a>
+                                <button type="submit" class="cursor-pointer hover:text-blue-900">Delete</a>
                             </form>
                         </div>
                     </td>
